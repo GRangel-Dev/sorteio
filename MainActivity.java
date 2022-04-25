9188aba767c9e2f507d847b64c7c359cfed374e8
GRangel-Dev <99808659+GRangel-Dev@users.noreply.github.com>
2022-04-25 14:56:58 -0300

    commit 1
    Código simples usando o comando Random().nextInt(valor entre as escolhas)

--- MainActivity.java
+++ MainActivity.java
@@ -3,6 +3,10 @@ package com.germanorangel.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void numeroSorteado(View view){
+        TextView texto = findViewById(R.id.textoNumeroSorteado);
+        int numero = new Random().nextInt(11);
+        texto.setText("Número: " + numero);
     }
 }
